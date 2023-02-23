917fb3ce1ea3d58dc7d03d097f91464381472540
blackyselva <selvam17196@gmail.com>
2023-02-23 23:21:25 +0530

    Adding.

---
+++ main.js
@@ -0,0 +1,17 @@
+let submit = document.querySelector("#submit");
+let form = document.querySelector("#form");
+
+form.addEventListener('submit', storeData);
+function storeData(e) {
+    e.preventDefault();
+    console.log("clicked");
+    let phNum = document.querySelector("#phNum").value;
+    let password = document.querySelector("#pass").value;
+    let fName = document.querySelector("#fName").value;
+    let lName = document.querySelector("#lName").value;
+
+    localStorage.setItem('phNumber', phNum);
+    localStorage.setItem('Password', password);
+    localStorage.setItem('first Name', fName);
+    localStorage.setItem('Surname', lName);
+}
